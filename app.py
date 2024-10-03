032d4dabe9aa5019e6d46cc275442f6e35354b72
Achraf Ait masasse <aitmasasseachraf@gmail.com>
2024-10-03 23:11:32 +0100

    Update

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ fields = [0] * grid_size
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global fields
+    warning_20 = None
+    warning_90 = None
+
     if request.method == 'POST':
         action = request.form.get('action')
         if action == 'Submit':
@@ -18,10 +21,20 @@ def index():
                 fields = [x + 1 for x in fields]
                 # Set the field under the submitted number to 0
                 fields[number] = 0
+
+                # Check if any of the specified fields reach or exceed 20
+                if any(fields[i] >= 20 for i in [7, 8, 9, 17, 18, 19, 27, 28, 29]):
+                    warning_20 = "Warning: [7, 8, 9, 17, 18, 19, 27, 28, 29] those numbers have reached or exceeded a value of 20!"
+
+                # Check if fields 28 and 29 reach or exceed 90
+                if fields[28] >= 90 or fields[29] >= 90:
+                    warning_90 = "Warning: Fields 28 or 29 have reached or exceeded a value of 90!"
+
         elif action == 'Clear':
             # Reset fields to initial state
             fields = [0] * grid_size
-    return render_template('index.html', fields=fields)
+
+    return render_template('index.html', fields=fields, warning_20=warning_20, warning_90=warning_90)
 
 if __name__ == '__main__':
     app.run(debug=True)

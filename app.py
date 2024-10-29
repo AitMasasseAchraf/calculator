86a7a0ee09216c25444d479a84abe35f10955bc9
Achraf Ait masasse <aitmasasseachraf@gmail.com>
2024-10-29 22:14:59 +0100

    Other Update

--- app.py
+++ app.py
@@ -67,23 +67,23 @@ def index():
 
                 for group_name, indices in threshold_70_groups.items():
                     group_values = [fields[i] for i in indices]
-                    if all(value % 70==0 and value !=0 for value in group_values):
+                    if all(value >=70 and value !=0 for value in group_values):
                         warnings_70[group_name] = f"{indices} "
 
                 # Additional condition checks
                 eight_times_group = [1, 2, 11, 12, 21, 31, 22, 32, 7, 17, 27, 8, 18, 28, 9, 19, 29]
-                if all(fields[i] % 8==0 and fields[i]!=0 for i in eight_times_group):
+                if all(fields[i] >=8 and fields[i]!=0 for i in eight_times_group):
                     additional_warnings["8_times"] = "[1, 2, 11, 12, 21, 31, 22, 32, 7, 17, 27, 8, 18, 28, 9, 19, 29]"
 
                 twohundred_times_group = [1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
                 one_list=[]
                 for i in twohundred_times_group:
-                    if fields[i] % 250==0 and fields[i]!=0 :
+                    if fields[i] >=250 and fields[i]!=0 :
                         one_list.append(i)
                 additional_warnings["250_time"]=one_list        
                 
                 forty_times_group = [7, 17, 27, 8, 18, 28, 9, 19, 29]
-                if all(fields[i] % 40==0 and fields[i]!=0 for i in forty_times_group):
+                if all(fields[i] >=40 and fields[i]!=0 for i in forty_times_group):
                     additional_warnings["40_times"] = " [7, 17, 27, 8, 18, 28, 9, 19, 29 ]"
 
                 hundred_fifty_times_groups = [
@@ -92,7 +92,7 @@ def index():
                 ]
                 hundred_list=[]
                 for i, group in enumerate(hundred_fifty_times_groups):
-                    if all(fields[idx] % 150==0 and fields[idx]!=0 for idx in group):
+                    if all(fields[idx] >=150 and fields[idx]!=0 for idx in group):
                         hundred_list.append(group)
                 additional_warnings["150_times"] = f" {str(hundred_list)} "
                 
@@ -106,7 +106,7 @@ def index():
                 }
                 for group_name, indices in threshold_50_groups.items():
                     group_values = [fields[i] for i in indices]
-                    if all(value % 50==0 and value !=0 for value in group_values):
+                    if all(value >=50 and value !=0 for value in group_values):
                         warnings_50[group_name] = f"{indices} "
 
                 threshold_25_groups ={
@@ -117,7 +117,7 @@ def index():
                 }
                 for group_name, indices in threshold_25_groups.items():
                     group_values = [fields[i] for i in indices]
-                    if all(value % 25==0 and value !=0 for value in group_values):
+                    if all(value >=25 and value !=0 for value in group_values):
                         warnings_25[group_name] = f"{indices} "        
 
         elif action == 'Clear':
